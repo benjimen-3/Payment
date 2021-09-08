70712a8b8edd1fee03a2fd12e10ceb657e35d8b3
benjimen <abenjimena@dbs.com>
2021-09-08 05:29:16 +0000

    added project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+import { HeaderComponent } from './header/header.component';
+import { NavitemsComponent } from './navitems/navitems.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TransactionComponent } from './transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+     HeaderComponent,
+     NavitemsComponent,
+     TransactionComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+      path:"login",component:LoginComponent  
+    },
+    {
+      path:"transaction", component:TransactionComponent
+    },
+    {
+      path:'', component:LoginComponent
+    }
+    
+
+  ])
+],
   providers: [],
   bootstrap: [AppComponent]
 })
